95078ab724182b982c694b4c1690f5af55dcf454
agatoneq <agata.sobczyk444@wp.pl>
2023-04-03 04:10:16 +0200

    zad1 koniec

--- Zadania.R
+++ Zadania.R
@@ -25,13 +25,13 @@ miary_sz_szczegolowego <- function(dane)
   # Miary rozproszenia
   wariancja = var(szereg_sz)
   odchylenie_standardowe = sd(szereg_sz)
- # wariancja = 
-  #odchylenie_standardowe = 
-  odchylenie_przecietne = 
-  odchylenie_przecietne_od_mediany = mad(szereg_sz)
-  odchylenie_cwiartkowe = IQR(szereg_sz)
-  wspolczynnik_zmiennosci =  100*sd(szereg_sz)/mean(szereg_sz)
-  pozycyjny_wspolczynnik_zmiennosci  =  100*IQR(szereg_sz)/median(szereg_sz)
+  wariancja_nieobciazona = wariancja * (n/(n-1))
+  odchylenie_standardowe_NO = sqrt(wariancja_nieobciazona)
+  odchylenie_przecietne = sum(abs(szereg_sz - srednia)) / length(szereg_sz)
+  odchylenie_przecietne_od_mediany = sum(abs(szereg_sz - mediana)) / length(szereg_sz)
+  odchylenie_cwiartkowe = IQR(szereg_sz)/2
+  wspolczynnik_zmiennosci =  100*odchylenie_standardowe/srednia
+  pozycyjny_wspolczynnik_zmiennosci  =  100*odchylenie_cwiartkowe/mediana
   
   # Miary asymetrii i koncentracji
   skosnosc = skewness(szereg_sz)
@@ -46,35 +46,101 @@ hist_salon1 <- hist(salon1, breaks = 8)
 hist_salon2 <- hist(salon1, breaks = 8)
 
 
+#funkcja dla kwantyli w szeregu rozdzielczym
+kwantyl<-function(szereg_rozdz, q)
+{
+  kwantyl=0
+  war_srodkowe <- szereg_rozdz$mids
+  liczebnosc <- szereg_rozdz$counts
+  
+  l_skumulowana <- liczebnosc
+  for(i in 1:length(liczebnosc))
+  {
+    l_skumulowana[i] = sum(liczebnosc[1:i])
+  }
+  
+  
+  if (sum(liczebnosc) %% 2 == 0)
+  {
+    indeks1 <- floor(sum(liczebnosc) * q)+1
+    indeks2 <- floor(sum(liczebnosc) * q)+2
+    
+    koniec1 = 0
+    i1 = 1
+    while (koniec1 == 0)
+    {
+      if (l_skumulowana[i1] > indeks1)
+      {
+        kwantyl1 <- war_srodkowe[i1]
+        koniec1 = 1
+      }
+      else
+        i1 = i1 + 1
+    }
+    koniec2 = 0
+    i2 = 1
+    while (koniec2 == 0)
+    {
+      if (l_skumulowana[i2] > indeks2)
+      {
+        kwantyl2 <- war_srodkowe[i2]
+        koniec2 = 1
+      }
+      else
+        i2 = i2 + 1
+    }
+    kwantyl = (kwantyl1+kwantyl2)/2
+  }
+  
+  else
+  {
+    indeks <- floor(sum(liczebnosc) * q)+1
+    
+    koniec = 0
+    i = 1
+    while (koniec == 0)
+    {
+      if (l_skumulowana[i] > indeks)
+      {
+        kwantyl = war_srodkowe[i]
+        koniec = 1
+      }
+      else
+        i = i + 1
+    }
+  }
+  return (kwantyl)
+}
+
+
 #MIARY WEDLUG SZEREGU ROZDZIELCZEGO
 miary_sz_rozdzielczego <- function(dane)
 {
   # utworzenie szeregu rozdzielczego dla salonu
   szereg_roz <- hist(dane, seq(min(dane), max(dane), length.out = 8), plot = FALSE)
   war_srodkowe <- szereg_roz$mids
-  liczebnosci <- szereg_roz$counts
-  weighted.mean(war_srodkowe, liczebnosci)
+  liczebnosc <- szereg_roz$counts
   
   # Miary przeciętne
-  srednia = weighted.mean(war_srodkowe, liczebnosci)
-  mediana = 
-  moda = 
-  Q1 = 
-  Q3 = 
-  
+  srednia = weighted.mean(war_srodkowe, liczebnosc)
+  mediana = kwantyl(szereg_roz,0.5)
+  moda = war_srodkowe[which.max(liczebnosc)]
+  Q1 = kwantyl(szereg_roz, 0.25)
+  Q3 = kwantyl(szereg_roz, 0.75)
+
   # Miary rozproszenia
-  wariancja = 
-  odchylenie_standardowe = 
-  # wariancja = 
-  #odchylenie_standardowe = 
-  odchylenie_przecietne = 
-  odchylenie_przecietne_od_mediany = 
-  odchylenie_cwiartkowe = 
-  wspolczynnik_zmiennosci = 
-  pozycyjny_wspolczynnik_zmiennosci = 
+  wariancja = sum(((war_srodkowe-srednia) ^ 2) * liczebnosc) / sum(liczebnosc)
+  odchylenie_standardowe = sqrt(wariancja)
+  wariancja_nieobciazona = wariancja * (n/(n-1))
+  odchylenie_standardowe_NO = sqrt(wariancja_nieobciazona)
+  odchylenie_przecietne = sum(abs(war_srodkowe - srednia) * liczebnosc) / sum(liczebnosc)
+  odchylenie_przecietne_od_mediany = sum(abs(war_srodkowe - mediana) * liczebnosc) / sum(liczebnosc)
+  odchylenie_cwiartkowe = (Q1 - Q3) / 2
+  wspolczynnik_zmiennosci = 100*odchylenie_standardowe/srednia
+  pozycyjny_wspolczynnik_zmiennosci = 100*odchylenie_cwiartkowe/mediana
   
   # Miary asymetrii i koncentracji
-  skosnosc = 
-  kurtoza = 
+  skosnosc = 3*(srednia - mediana) / odchylenie_standardowe
+  kurtoza = (sum((war_srodkowe - srednia) ^ 4 * liczebnosc) / sum(liczebnosc)) / (odchylenie_standardowe ^ 4)
   eksces = kurtoza - 3
 }
