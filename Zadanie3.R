e5788d165badb810d3ae90635266468d36506277
agatoneq <agata.sobczyk444@wp.pl>
2023-05-15 11:38:50 +0200

    zad3

--- Zadanie3.R
+++ Zadanie3.R
@@ -1,12 +1,3 @@
-sd_ <- function(x) 
-{
-  n <- length(x)
-  srednia <- mean(x) 
-  suma <- sum((x - srednia)^2)        
-  odchylenie <- sqrt(suma / (n - 1))
-  return(odchylenie)
-}
-
 zadanie3 <- function(salon1)
 {
 v <- length(salon1) 
@@ -14,8 +5,8 @@ v <- length(salon1)
 x_bar <- mean(salon1) 
 cat("wartość średnia",x_bar, "\n")
 
-os<-sd_(salon1)
-cat("odchylenie standardowe",sd_(salon1), "\n")
+os<-sd(salon1)
+cat("odchylenie standardowe",sd(salon1), "\n")
 
 lsw <- v-1
 cat("liczba stopni swobody ",lsw, "\n") 
@@ -33,4 +24,3 @@ if(wk>t || -wk<t)   {
   cat("Brak podstaw do odrzucenia hipoztezy zerowej ")
 }
 }
-
